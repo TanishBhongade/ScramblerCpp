39c7589579767c2269065f4ba7fa568775ac7005
Tanish Bhongade <tanishbhongade@gmail.com>
2021-07-04 22:28:31 +0530

    Initial commit

---
+++ Scrambler.cpp
@@ -0,0 +1,67 @@
+/*
+Author: Tanish Bhongade
+Date: 4 Jul 2021
+*/
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+#include<cstring>
+
+using namespace std;
+
+const int scramLen = 25;
+char mainScramble[scramLen][3];
+string moves="RULFDU";
+char dir[]={" 2'"};
+
+int getRand(int upr, int lwr){
+    int randNum;
+    randNum=(rand() % (upr - lwr + 1)) + lwr;
+    return randNum;
+}
+
+void initScramble(){
+    for(int loop=0; loop<scramLen; loop++){
+        strcpy(mainScramble[loop],"OO");
+    }
+}
+
+void scrambleReplace(){
+    for(int loop=0; loop<scramLen; loop++){
+        mainScramble[loop][0]=moves[getRand(5,0)];
+        mainScramble[loop][1]=dir[getRand(2,0)];
+    }
+}
+
+void valid(){
+    int loopOne, loopTwo;
+    for(loopOne=1; loopOne<scramLen; loopOne++){
+        while(mainScramble[loopOne][0] == mainScramble[loopOne-1][0]){
+            mainScramble[loopOne][0] = moves[getRand(5,0)];
+        }
+    }
+    for(loopTwo=2; loopTwo<scramLen; loopTwo++){
+        while((mainScramble[loopTwo][0] == mainScramble[loopTwo-2][0]) || (mainScramble[loopTwo][0] == mainScramble[loopTwo-1][0])){
+            mainScramble[loopTwo][0] = moves[getRand(5,0)];
+        }
+    }
+}
+
+void printData(){
+    int loop=0;
+    for(loop=0; loop<scramLen; loop++){
+        cout << mainScramble [loop] << " ";
+    }
+}
+
+int main(){
+    srand(time(0));
+    cout << "3x3 Scramble: \n";
+    
+    initScramble();
+    scrambleReplace();
+    valid();
+    printData();
+    
+    return 0;
+}
